786b32b3a3f88b6ba504e6b7582ccead8aeca5b0
Eric Hosford <eric.hosford@shadowhealth.com>
2018-08-30 15:51:47 -0400

    Add delete to task

--- skills/sample_taskbot.js
+++ skills/sample_taskbot.js
@@ -18,116 +18,122 @@
 
 */
 
-module.exports = function(controller) {
+module.exports = function (controller) {
 
-    // listen for someone saying 'tasks' to the bot
-    // reply with a list of current tasks loaded from the storage system
-    // based on this user's id
-    controller.hears(['tasks','todo'], 'direct_message', function(bot, message) {
+  // listen for someone saying 'tasks' to the bot
+  // reply with a list of current tasks loaded from the storage system
+  // based on this user's id
+  controller.hears(['tasks', 'todo'], 'direct_message', function (bot, message) {
 
-        // load user from storage...
-        controller.storage.users.get(message.user, function(err, user) {
+    // load user from storage...
+    controller.storage.users.get(message.user, function (err, user) {
 
-            // user object can contain arbitary keys. we will store tasks in .tasks
-            if (!user || !user.tasks || user.tasks.length == 0) {
-                bot.reply(message, 'There are no tasks on your list. Say `add _task_` to add something.');
-            } else {
+      // user object can contain arbitary keys. we will store tasks in .tasks
+      if (!user || !user.tasks || user.tasks.length == 0) {
+        bot.reply(message, 'There are no tasks on your list. Say `add _task_` to add something.');
+      } else {
 
-                var text = 'Here are your current tasks: \n' +
-                    generateTaskList(user) +
-                    'Reply with `done _number_` to mark a task completed.';
+        var text = 'Here are your current tasks: \n' +
+          generateTaskList(user) +
+          'Reply with `done _number_` to mark a task completed.';
 
-                bot.reply(message, text);
+        bot.reply(message, text);
 
-            }
-
-        });
+      }
 
     });
 
-    // listen for a user saying "add <something>", and then add it to the user's list
-    // store the new list in the storage system
-    controller.hears(['add (.*)'],'direct_message,direct_mention,mention', function(bot, message) {
+  });
 
-        var newtask = message.match[1];
-        controller.storage.users.get(message.user, function(err, user) {
+  // listen for a user saying "add <something>", and then add it to the user's list
+  // store the new list in the storage system
+  controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', function (bot, message) {
 
-            if (!user) {
-                user = {};
-                user.id = message.user;
-                user.tasks = [];
-            }
+    var newtask = message.match[1];
+    controller.storage.users.get(message.user, function (err, user) {
 
-            user.tasks.push(newtask);
+      if (!user) {
+        user = {};
+        user.id = message.user;
+        user.tasks = [];
+      }
 
-            controller.storage.users.save(user, function(err,saved) {
+      user.tasks.push(newtask);
 
-                if (err) {
-                    bot.reply(message, 'I experienced an error adding your task: ' + err);
-                } else {
-                    bot.api.reactions.add({
-                        name: 'thumbsup',
-                        channel: message.channel,
-                        timestamp: message.ts
-                    });
-                }
+      controller.storage.users.save(user, function (err, saved) {
 
-            });
-        });
+        if (err) {
+          bot.reply(message, 'I experienced an error adding your task: ' + err);
+        } else {
+          bot.api.reactions.add({
+            name: 'thumbsup',
+            channel: message.channel,
+            timestamp: message.ts
+          });
+        }
 
+      });
     });
 
-    // listen for a user saying "done <number>" and mark that item as done.
-    controller.hears(['done (.*)'],'direct_message', function(bot, message) {
+  });
 
-        var number = message.match[1];
+  // listen for a user saying "done <number>" and mark that item as done.
+  controller.hears(['done (.*)'], 'direct_message', function (bot, message) {
 
-        if (isNaN(number)) {
-            bot.reply(message, 'Please specify a number.');
+    var number = message.match[1];
+
+    if (isNaN(number)) {
+      bot.reply(message, 'Please specify a number.');
+    } else {
+
+      // adjust for 0-based array index
+      number = parseInt(number) - 1;
+
+      controller.storage.users.get(message.user, function (err, user) {
+
+        if (!user) {
+          user = {};
+          user.id = message.user;
+          user.tasks = [];
+        }
+
+        if (number < 0 || number >= user.tasks.length) {
+          bot.reply(message, 'Sorry, your input is out of range. Right now there are ' + user.tasks.length + ' items on your list.');
         } else {
 
-            // adjust for 0-based array index
-            number = parseInt(number) - 1;
+          var item = user.tasks.splice(number, 1);
 
-            controller.storage.users.get(message.user, function(err, user) {
+          // reply with a strikethrough message...
+          bot.reply(message, '~' + item + '~');
 
-                if (!user) {
-                    user = {};
-                    user.id = message.user;
-                    user.tasks = [];
-                }
+          controller.storage.users.delete(user, function (err, deleted) {
+            if (err) {
+              bot.reply(message, 'I experienced an error deleting your task: ' + err);
+            }
 
-                if (number < 0 || number >= user.tasks.length) {
-                    bot.reply(message, 'Sorry, your input is out of range. Right now there are ' + user.tasks.length + ' items on your list.');
-                } else {
-
-                    var item = user.tasks.splice(number,1);
-
-                    // reply with a strikethrough message...
-                    bot.reply(message, '~' + item + '~');
-
-                    if (user.tasks.length > 0) {
-                        bot.reply(message, 'Here are our remaining tasks:\n' + generateTaskList(user));
-                    } else {
-                        bot.reply(message, 'Your list is now empty!');
-                    }
-                }
-            });
+            if (user.tasks.length > 0) {
+              bot.reply(message, 'Here are our remaining tasks:\n' + generateTaskList(user));
+            } else {
+              bot.reply(message, 'Your list is now empty!');
+            }
+          });
         }
-
-    });
-
-    // simple function to generate the text of the task list so that
-    // it can be used in various places
-    function generateTaskList(user) {
-
-        var text = '';
-
-        for (var t = 0; t < user.tasks.length; t++) {
-            text = text + '> `' +  (t + 1) + '`) ' +  user.tasks[t] + '\n';
-        }
-
-        return text;
-
+      });
     }
+
+  });
+
+  // simple function to generate the text of the task list so that
+  // it can be used in various places
+  function generateTaskList(user) {
+
+    var text = '';
+
+    for (var t = 0; t < user.tasks.length; t++) {
+      text = text + '> `' + (t + 1) + '`) ' + user.tasks[t] + '\n';
+    }
+
+    return text;
+
+  }
 }
